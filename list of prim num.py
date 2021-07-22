f0738623a453ac497a648560ae3e7910d9422706
YashShah25 <87748033+YashShah25@users.noreply.github.com>
2021-07-22 12:34:56 +0530

    Add files via upload

---
+++ list of prim num.py
@@ -0,0 +1,12 @@
+def prim(no):
+    for i in range(2,no):
+        if no%i==0:
+            return False
+    else:
+        return True
+
+num = int(input("Enter num:"))
+prim_lst = filter(prim, range(0,num))
+
+print("list of prim numbers is\n")
+for temp in prim_lst:print(temp)
